b44aaa727b6d24294ffd05e7db60991447092c9b
Sweet-ka <sweet-ka@mail.ru>
2022-06-11 14:08:31 +0300

    init node js

--- NSP/index.js
+++ NSP/index.js
@@ -5,3 +5,21 @@
 import { module } from './pages/module.js';
 
 module.start();
+
+const mysql = require("mysql2");
+ 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "usersdb",
+  password: "пароль_от_сервера"
+});
+ 
+connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+});
